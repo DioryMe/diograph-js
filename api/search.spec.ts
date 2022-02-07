7cfea307c420cebd1b36552cfe2f400164e5cbd6
Jouni Alanen <jouni@diory.me>
2022-02-07 13:35:22 +0200

    Spec for search and uses fixtures/diograph.json file

---
+++ api/search.spec.ts
@@ -0,0 +1,20 @@
+import { DiographJson } from '../diograph'
+
+describe('get', () => {
+  let diographJson: DiographJson
+
+  beforeEach(async () => {
+    diographJson = new DiographJson({ path: 'fixtures/diograph.json' })
+    await diographJson.load()
+  })
+
+  it('text search works', () => {
+    const expectedResults = [diographJson.get('generic-content')]
+    expect(diographJson.search('generic', 'text')).toEqual(expectedResults)
+  })
+
+  it('data search works', () => {
+    const expectedResults = [diographJson.get('5456c2c3-4a69-4d80-bd2f-caa9945cff71')]
+    expect(diographJson.search('VideoObject', 'data')).toEqual(expectedResults)
+  })
+})
