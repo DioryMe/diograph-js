a1c0e8f84dd327a393fbd8c912a20b940efac9cf
Jouni Alanen <jouni@diory.me>
2022-02-09 12:07:47 +0200

    dryRun options - doesn't delete diory if { dryRun: true } - initial deleteOptions for deleteDiory

--- api/delete.spec.ts
+++ api/delete.spec.ts
@@ -1,35 +1,43 @@
 import { Diory, Diograph } from '../types'
 import { DiographJson } from '../diograph'
 
+const diory: Diory = {
+  id: 'some-id',
+}
+
+const diory2: Diory = {
+  id: 'some-other-id',
+}
+
 describe('deleteDiory', () => {
-  const diory: Diory = {
-    id: 'some-id',
-  }
+  let diographJson: DiographJson
 
-  const diory2: Diory = {
-    id: 'some-other-id',
-  }
+  beforeEach(() => {
+    const diograph: Diograph = {
+      'some-id': diory,
+      'some-other-id': diory2,
+    }
+    diographJson = new DiographJson({ path: 'some-path/diograph.json' })
+    diographJson.setDiograph(diograph)
+  })
 
-  describe('delete', () => {
-    let diographJson: DiographJson
-
-    beforeEach(() => {
-      const diograph: Diograph = {
-        'some-id': diory,
-        'some-other-id': diory2,
-      }
-      diographJson = new DiographJson({ path: 'some-path/diograph.json' })
-      diographJson.setDiograph(diograph)
-    })
-
-    it('works', () => {
-      const returnValue = diographJson.deleteDiory('some-id')
-      expect(returnValue).toEqual(true)
+  describe('without options', () => {
+    it('deletes single diory', () => {
+      const returnValue = diographJson.deleteDiory(diory.id)
+      expect(returnValue).toEqual([diory])
 
       expect(diographJson.get('some-id')).toEqual(undefined)
       expect(diographJson.get('some-other-id')).toEqual(diory2)
-      expect(diographJson.diograph['some-id']).toEqual(undefined)
-      expect(diographJson.diograph['some-other-id']).toEqual(diory2)
+    })
+  })
+
+  describe('with dryRun enabled', () => {
+    it("doesn't delete single diory", () => {
+      const returnValue = diographJson.deleteDiory(diory.id, { dryRun: true })
+      expect(returnValue).toEqual([diory])
+
+      expect(diographJson.get('some-id')).toEqual(diory)
+      expect(diographJson.get('some-other-id')).toEqual(diory2)
     })
   })
 })
--- api/delete.ts
+++ api/delete.ts
@@ -1,8 +1,31 @@
 import { DiographJson } from '../diograph'
 import { Diory } from '../types'
 
-function deleteDiory(this: DiographJson, id: string): boolean {
-  return delete this.diograph[id]
+interface deleteOptions {
+  force: boolean
+  linkedDiories: boolean
+  dryRun: boolean
+}
+
+const DEFAULT_OPTIONS: deleteOptions = {
+  // deletes linkedDiories although they are linked
+  // without this throws an error if any diories are linked to other diories
+  force: false,
+  linkedDiories: false,
+  dryRun: false,
+}
+
+function deleteDiory(this: DiographJson, id: string, opts: object = {}): Array<Diory> {
+  const optsWithDefaults: deleteOptions = {
+    ...DEFAULT_OPTIONS,
+    ...opts,
+  }
+  const dioryToBeDeleted = this.diograph[id]
+  if (!optsWithDefaults.dryRun) {
+    delete this.diograph[id]
+    return [dioryToBeDeleted]
+  }
+  return [dioryToBeDeleted]
 }
 
 export { deleteDiory }
