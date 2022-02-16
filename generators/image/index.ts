74f34a064bc464e81d25a6e44f930b4a9607dd5a
Jouni Alanen <jouni@diory.me>
2022-02-16 22:12:39 +0200

    Move image-reader.js from diory-electron-browser to generators/image/metadata.ts

--- generators/image/index.ts
+++ generators/image/index.ts
@@ -3,13 +3,10 @@ import { generateThumbnail } from './thumbnailer'
 
 async function dioryImageGenerator(fileContent: Buffer, filePath: string, contentUrl: string) {
   const thumbnailBuffer = await generateThumbnail(fileContent)
-  const schema = {
-    '@context': 'https://schema.org',
-    '@type': 'ImageObject',
-    contentUrl,
-    ...retrieveMetadata(),
-  }
-  return { typeSpecificDiory: { data: [schema] }, thumbnailBuffer, cid: 'sadfasdf' }
+
+  const typeSpecificDiory = await retrieveMetadata(filePath)
+
+  return { typeSpecificDiory, thumbnailBuffer, cid: 'sadfasdf' }
 }
 
 export { dioryImageGenerator }
--- generators/video/index.ts
+++ generators/video/index.ts
@@ -2,10 +2,12 @@ import { generateThumbnail } from './thumbnailer'
 
 async function dioryVideoGenerator(fileContent: Buffer, filePath: string, contentUrl: string) {
   const { thumbnailBuffer, ffmpegOutput } = await generateThumbnail(filePath)
+
   const creationTime = ffmpegOutput.match(/(?<=creation_time\s\s\s:\s).*/)
   const latlng = ffmpegOutput.match(/(?<=location\s\s\s\s\s\s\s\s:\s\+).{16}/)
   // Hacky way to trim longitude's leading zero
   const splitter = latlng && latlng[0].match(/\+0/) ? '+0' : '0'
+
   const typeSpecificDiory = {
     date: creationTime && creationTime[0],
     latlng: latlng && latlng[0].split(splitter).join(', '),
@@ -17,6 +19,7 @@ async function dioryVideoGenerator(fileContent: Buffer, filePath: string, conten
       },
     ],
   }
+
   return { typeSpecificDiory, thumbnailBuffer, cid: 'sadfasdf' }
 }
 
