536d963bb5b00dbb6d34be42f9a009e891a29b9a
Jouni Alanen <jouni@diory.me>
2022-02-17 11:35:39 +0200

    Add also duration to videoDiory.data

--- generators/video/index.ts
+++ generators/video/index.ts
@@ -5,6 +5,7 @@ async function dioryVideoGenerator(fileContent: Buffer, filePath: string, conten
 
   const creationTime = ffmpegOutput.match(/(?<=creation_time\s\s\s:\s).*/)
   const latlng = ffmpegOutput.match(/(?<=location\s\s\s\s\s\s\s\s:\s\+).{16}/)
+  const duration = ffmpegOutput.match(/(?<=Duration:\s).{11}/)
   // Hacky way to trim longitude's leading zero
   const splitter = latlng && latlng[0].match(/\+0/) ? '+0' : '0'
 
@@ -16,6 +17,7 @@ async function dioryVideoGenerator(fileContent: Buffer, filePath: string, conten
         '@context': 'https://schema.org',
         '@type': 'VideoObject',
         contentUrl,
+        duration: duration && duration[0],
         encodingFormat: '',
       },
     ],
