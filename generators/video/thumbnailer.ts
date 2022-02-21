4d9f7fe7b6cf9c847466a661f2c7c5a53029e6ac
Jouni Alanen <jouni@diory.me>
2022-02-21 14:17:22 +0200

    Remove reference to removed ffmpeg-static dependency

--- generators/video/thumbnailer.ts
+++ generators/video/thumbnailer.ts
@@ -1,13 +1,12 @@
 // Promisified version of execFile to execute the ffmpeg command safely as child_process
 const execFile = require('util').promisify(require('child_process').execFile)
-const ffmpegStaticPath = require('ffmpeg-static')
 import { join } from 'path'
 import { readFile } from 'fs/promises'
 import { v4 as uuidv4 } from 'uuid'
 
 async function generateThumbnail(sourceFilePath: string, time: number = 3) {
   const tmpPath = join('/', 'tmp', `${uuidv4()}.jpg`)
-  const pathToFfmpeg = process.env.FFMPEG_PATH || ffmpegStaticPath
+  const pathToFfmpeg = process.env.FFMPEG_PATH
   // prettier-ignore
   return execFile(pathToFfmpeg, [
     '-y',
