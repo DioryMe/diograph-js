0fc3f29006e82f0da3bd586699702208b09dd9af
Jouni Alanen <jouni@diory.me>
2022-02-21 10:37:24 +0200

    Enable setting ffmpegPath with env FFMPEG_PATH (Electron binary requires this)

--- generators/video/thumbnailer.ts
+++ generators/video/thumbnailer.ts
@@ -1,12 +1,13 @@
 // Promisified version of execFile to execute the ffmpeg command safely as child_process
 const execFile = require('util').promisify(require('child_process').execFile)
-const pathToFfmpeg = require('ffmpeg-static')
+const ffmpegStaticPath = require('ffmpeg-static')
 import { join } from 'path'
 import { readFile } from 'fs/promises'
 import { v4 as uuidv4 } from 'uuid'
 
 async function generateThumbnail(sourceFilePath: string, time: number = 3) {
   const tmpPath = join('/', 'tmp', `${uuidv4()}.jpg`)
+  const pathToFfmpeg = process.env.FFMPEG_PATH || ffmpegStaticPath
   // prettier-ignore
   return execFile(pathToFfmpeg, [
     '-y',
@@ -15,7 +16,7 @@ async function generateThumbnail(sourceFilePath: string, time: number = 3) {
     '-an',
     '-ss', time,
     tmpPath
-  ]).then((async (returnObject: any) => {
+  ]).then(async (returnObject: any) => {
     const ffmpegOutput: string = returnObject.stderr
     let thumbnailBuffer
     try {
