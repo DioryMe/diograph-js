f162b963996e8e4ba879703bc79d3ac09311f340
Jouni Alanen <jouni@diory.me>
2022-02-21 16:01:36 +0200

    Rotate image thumbnails correctly

--- generators/image/thumbnailer.ts
+++ generators/image/thumbnailer.ts
@@ -5,6 +5,7 @@ const MAX_WIDTH = 480
 
 const generateThumbnail = async (fileContent: Buffer) => {
   return await sharp(fileContent)
+    .rotate()
     .resize({ width: MAX_WIDTH, height: MAX_HEIGHT, fit: 'inside' })
     // TODO: Detect file format: .png, .gif etc.
     .jpeg()
