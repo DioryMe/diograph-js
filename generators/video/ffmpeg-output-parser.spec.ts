18b6c343dfdac25e0ba16c36ffa74587e23cd3f8
Jouni Alanen <jouni@diory.me>
2022-02-23 16:03:18 +0200

    Add .mp4 location case

--- generators/video/ffmpeg-output-parser.spec.ts
+++ generators/video/ffmpeg-output-parser.spec.ts
@@ -4,4 +4,8 @@ describe('parseLatlng', () => {
   it('fixture location', async () => {
     expect(parseLatlng('    location        : +65.4752+027.9785/')).toEqual('65.4752, 27.9785')
   })
+
+  it('mp4 location', async () => {
+    expect(parseLatlng('    location        : +62.2273+25.8120/')).toEqual('62.2273, 25.8120')
+  })
 })
