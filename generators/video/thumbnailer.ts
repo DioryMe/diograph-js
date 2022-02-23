7b75020d617e0ee0f2ef02a663b0e5635771ca90
Jouni Alanen <jouni@diory.me>
2022-02-23 10:30:28 +0200

    Throw error when FFMPEG_PATH missing - when calling video/generateThumbnail - test for it - initial tests for video/thumbnailer

--- generators/video/thumbnailer.ts
+++ generators/video/thumbnailer.ts
@@ -36,6 +36,9 @@ function executeFfmpegInChildProcess(
 
 async function generateThumbnail(sourceFilePath: string, time: number = 3) {
   const pathToFfmpeg = process.env.FFMPEG_PATH
+  if (!pathToFfmpeg) {
+    throw new Error('FFMPEG_PATH not defined, video/generateThumbnail requires it')
+  }
   return executeFfmpegInChildProcess(pathToFfmpeg, sourceFilePath, time).then(
     async ({ returnObject, tmpPath }) => {
       const ffmpegOutput: string = returnObject.stderr
