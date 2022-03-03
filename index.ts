e5f242f86b63697e3e04baffbf3fe5c8566fc856
Jouni Alanen <jouni@diory.me>
2022-03-03 10:01:17 +0200

    Add imageThumbnailer and dioryVideoGenerator to diograph-js imports

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { DiographJson } from './diograph'
 import { Room } from './room'
 import { LocalConnector } from './connectors'
+import { generateThumbnail as imageThumbnailer } from './generators/image/thumbnailer'
+import { dioryVideoGenerator } from './generators/video'
 
-export { DiographJson, Room, LocalConnector }
+export { DiographJson, Room, LocalConnector, imageThumbnailer, dioryVideoGenerator }
