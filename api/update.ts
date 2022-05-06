46a70181466dc819ab429533ec70d56e835cbfb7
Jouni Alanen <jouni@diory.me>
2022-05-06 09:58:01 +0300

    api/update converted to use Diory objects: tests pass

--- api/update.ts
+++ api/update.ts
@@ -1,16 +1,18 @@
 import { Diograph } from '../diograph'
-import { DioryAttributes, DioryObject } from '../types'
+import { DioryAttributes } from '../types'
+import { Diory } from '../diory'
 
-function update(this: Diograph, id: string, payload: DioryAttributes): DioryObject {
+function update(this: Diograph, id: string, payload: DioryAttributes): Diory {
   // Check that only valid keys are given
   Object.keys(payload).forEach((key) => {
     if (!['text', 'image', 'latlng', 'date', 'data', 'style'].includes(key))
       throw new Error('extra key')
   })
 
-  let diory = { ...this.diograph[id], ...payload }
-  this.diograph[id] = diory
-  return this.getDiory(id)
+  const diory = this.getDiory2(id)
+  diory.dioryAttributes = { ...diory.dioryAttributes, ...payload }
+  diory.extractDioryAttributes(diory.dioryAttributes)
+  return this.getDiory2(id)
 }
 
 export { update }
