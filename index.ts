35e46b8a917de84806233a9d39d447031f2ccd97
Jouni Alanen <jouni@diory.me>
2022-05-19 08:53:26 +0300

    Add Connection to index.ts

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ import { Diograph } from './diograph'
 import { Room } from './room'
 import { Diory } from './diory'
 import { RoomClient } from './clients/roomClient'
+import { Connection } from './connection'
 
-export { Diograph, Room, Diory, RoomClient }
+export { Diograph, Room, Diory, RoomClient, Connection }
