f83257e98041d397abf16b4265b72728ae6e65dc
Jouni Alanen <jouni@diory.me>
2022-05-19 08:54:52 +0300

    Add contentSourceClient

---
+++ clients/contentSourceClient.ts
@@ -0,0 +1,56 @@
+import { join } from 'path'
+import { Connection } from '../connection'
+import { Diory } from '../diory'
+import { ElectronClient } from './electronClient'
+
+class ContentSourceClient {
+  address: string
+  connection?: Connection
+  client: ElectronClient
+
+  constructor(config: any, connection?: Connection, client?: ElectronClient) {
+    this.address = config.address
+    this.connection = connection
+    this.client = client || new ElectronClient()
+  }
+
+  getFilePath = (contentUrl: string) => {
+    return join(this.address, contentUrl)
+  }
+
+  getContentUrl = (diory: string) => {
+    // TODO: Derive contentUrl from diory
+    return join(this.address, diory)
+  }
+
+  addContent = async (fileContent: Buffer | string, diory?: string) => {
+    let filePath
+    if (diory) {
+      filePath = this.getContentUrl(diory)
+    } else {
+      filePath = this.getContentUrl(Date.now().toString())
+    }
+
+    await this.client.writeItem(filePath, fileContent)
+
+    const contentUrl = filePath // makeRelative(filePath, this.address)
+    this.connection?.addContentUrl(contentUrl, contentUrl, new Diory({ id: '123' }))
+
+    return contentUrl
+  }
+
+  readContent = async (contentUrl: string) => {
+    const filePath: string = this.getFilePath(contentUrl)
+    if (!filePath) {
+      throw new Error('Nothing found with that contentUrl!')
+    }
+    return this.client.readItem(filePath)
+  }
+
+  deleteContent = async (contentUrl: string) => {
+    const filePath: string = this.getFilePath(contentUrl)
+    return this.client.deleteItem(filePath)
+  }
+}
+
+export { ContentSourceClient }
