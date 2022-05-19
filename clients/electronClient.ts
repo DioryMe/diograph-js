c57372fe33f38fd7565424ff03dceab664b8328a
Jouni Alanen <jouni@diory.me>
2022-05-19 09:53:51 +0300

    Cleanup

--- clients/electronClient.ts
+++ clients/electronClient.ts
@@ -1,10 +1,3 @@
-// TODO: Make alternative to this which uses Electron Local API actions
-// - requires bare bones app with Electron Local API backend to work
-// => skeleton for any diograph-js Electron app
-
-import { Diory } from '..'
-import { Diograph } from '../diograph'
-
 declare global {
   interface Window {
     channelsApi: any
