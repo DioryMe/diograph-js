848bd43b17036c70562e0ea18f8ae57d74084666
Jouni Alanen <jouni@diory.me>
2022-06-24 15:33:45 +0300

    Remove ElectronServer from index.ts to get diograph-browser-electron build to pass

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import { Room } from './core/room'
 import { Diory } from './core/diory'
 import { RoomClient } from './clients/roomClient'
 import { ElectronClient } from './clients/electronClient'
-import { ElectronServer } from './clients/electronServer'
 import { ElectronClientMock } from './clients/electronClientMock'
 import { Connection } from './core/connection'
 import { DioryAttributes, DioryGeneratorData, DioryObject, DioryLinkObject } from './types'
@@ -15,7 +14,6 @@ export {
   RoomClient,
   Connection,
   ElectronClient,
-  ElectronServer,
   ElectronClientMock,
   DioryAttributes,
   DioryGeneratorData,
