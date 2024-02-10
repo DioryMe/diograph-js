00208fcb7de8ecbd7a48ec332ae980c868733816
Jouni Alanen <jouni@diory.me>
2024-02-10 10:21:41 +0200

    Refactor & extend utils.spec for allKeysExist and allMatchToQuery

--- utils/utils.spec.ts
+++ utils/utils.spec.ts
@@ -1,21 +1,69 @@
 import { IDioryObject } from '../types'
-import { allKeysExist } from './utils'
+import { allKeysExist, allMatchToQuery } from './utils'
 
 describe('allKeysExist()', () => {
   describe('given text query', () => {
     let allKeysExistWithTextQuery: (dioryObject: IDioryObject) => boolean
     beforeEach(() => {
-      allKeysExistWithTextQuery = allKeysExist({ text: 'some-text' })
+      const query = { text: 'some-text' }
+      allKeysExistWithTextQuery = allKeysExist(query)
     })
 
     it('returns true with text prop', () => {
-      const result = allKeysExistWithTextQuery({ id: 'some-id', text: 'other-text' })
+      const diory = { id: 'some-id', text: 'other-text' }
+      const result = allKeysExistWithTextQuery(diory)
 
       expect(result).toBe(true)
     })
 
     it('returns false without text prop', () => {
-      const result = allKeysExistWithTextQuery({ id: 'some-id' })
+      const diory = { id: 'some-id' }
+      const result = allKeysExistWithTextQuery(diory)
+
+      expect(result).toBe(false)
+    })
+  })
+})
+describe('allMatchToQuery()', () => {
+  describe('given query with id and text', () => {
+    let allMatchToQueryWithTextQuery: (dioryObject: IDioryObject) => boolean
+    beforeEach(() => {
+      const query = { id: 'some-id', text: 'some-text' }
+      allMatchToQueryWithTextQuery = allMatchToQuery(query)
+    })
+
+    it('returns true if id and text matches', () => {
+      const diory = { id: 'some-id', text: 'some-text' }
+      const result = allMatchToQueryWithTextQuery(diory)
+
+      expect(result).toBe(true)
+    })
+
+    it('returns false if only text matches', () => {
+      const diory = { id: 'other-id', text: 'some-text' }
+      const result = allMatchToQueryWithTextQuery(diory)
+
+      expect(result).toBe(false)
+    })
+  })
+
+  describe('given query with text only', () => {
+    let allMatchToQueryWithTextQuery: (dioryObject: IDioryObject) => boolean
+    beforeEach(() => {
+      const query = { text: 'some-text' }
+      allMatchToQueryWithTextQuery = allMatchToQuery(query)
+    })
+
+    it('returns true if text matches', () => {
+      const diory = { id: 'some-id', text: 'some-text' }
+      const result = allMatchToQueryWithTextQuery(diory)
+
+      expect(result).toBe(true)
+    })
+
+    it("returns false if text doesn't match", () => {
+      const diory = { id: 'some-id', text: 'other-text' }
+      const result = allMatchToQueryWithTextQuery(diory)
 
       expect(result).toBe(false)
     })
