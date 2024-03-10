571ba3dc39d7923decbed1910eefa3144bef496e
Olli-Pekka Pohjola <op@diory.me>
2024-03-10 21:03:24 +0200

    Rename diories (#21)

---
+++ utils/queryDiories.spec.ts
@@ -0,0 +1,17 @@
+import { queryDiories } from './queryDiories'
+
+describe('queryDiories()', () => {
+  describe('given text query', () => {
+    it('returns diories with matching text', () => {
+      const result = queryDiories({ text: 'some-text' },{ 'some-id': { id: 'some-id', text: 'some-text' }})
+
+      expect(result).toStrictEqual({ 'some-id': { id: 'some-id', text: 'some-text' }})
+    })
+
+    it('does not return diories with non-matching text', () => {
+      const result = queryDiories({ text: 'other-text' },{ 'some-id': { id: 'some-id', text: 'some-text' }})
+
+      expect(result).toStrictEqual({})
+    })
+  })
+})
