5fc7d1e21d1c9d96bebc05138f1de0d8e2577cb9
Jouni Alanen <jouni@diory.me>
2024-04-21 08:39:44 +0300

    Extend validator with other schemas

--- diograph/validator.ts
+++ diograph/validator.ts
@@ -3,7 +3,7 @@ const ajv = new Ajv({ allErrors: true })
 // const addFormats = require('ajv-formats')
 // addFormats(ajv)
 
-const schema = {
+const diographSchema = {
   $schema: 'http://json-schema.org/draft-07/schema#',
   type: 'object',
   additionalProperties: {
@@ -53,13 +53,62 @@ const schema = {
   // required: ['/'],
 }
 
-export const validateDiograph = (diographObject: object) => {
+const cidMappingSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  type: 'object',
+  additionalProperties: {
+    type: 'string',
+  },
+}
+
+const roomConfigDataSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  type: 'object',
+  required: ['address', 'clientType'],
+  properties: {
+    address: { type: 'string' },
+    clientType: { type: 'string', enum: ['LocalClient', 'S3Client'] },
+  },
+  additionalProperties: {
+    id: { type: 'string' },
+  },
+}
+
+const connectionConfigDataSchema = roomConfigDataSchema
+
+const connectionDataSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  type: 'object',
+  required: ['address'],
+  properties: {
+    address: { type: 'string' },
+  },
+  additionalProperties: {
+    id: { type: 'string' },
+    diograph: diographSchema,
+  },
+}
+
+const validate = (schema: object, objectToValidate: object) => {
   const validate = ajv.compile(schema)
 
-  const isValid = validate(diographObject)
+  const isValid = validate(objectToValidate)
 
   if (!isValid) {
-    console.log('Diograph is not valid: ' + JSON.stringify(validate.errors))
-    throw new Error('Diograph is not valid: ' + JSON.stringify(validate.errors))
+    console.log('Object is not valid based on schema: ' + JSON.stringify(validate.errors))
+    throw new Error('Object is not valid based on schema: ' + JSON.stringify(validate.errors))
   }
+}
+
+const validateDiograph = (diographObject: object) => {
+  validate(diographSchema, diographObject)
+}
+
+export {
+  validateDiograph,
+  cidMappingSchema,
+  roomConfigDataSchema,
+  connectionConfigDataSchema,
+  connectionDataSchema,
+  validate,
 }
