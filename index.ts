abd2290dcbafa632fc40cdbf2ed16ba7a432304b
Olli-Pekka Pohjola <op@diory.me>
2024-04-21 22:03:10 +0300

    Refactor error messages

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 export { Diograph } from './diograph/diograph'
 export { Diory } from './diory/diory'
-export { IDiograph, IDiographObject, IDiory, IDioryObject, IDioryProps, ILinkObject } from './types'
+export {
+  IDiograph,
+  IDiographObject,
+  IDiory,
+  IDioryObject,
+  IDioryProps,
+  ILinkObject,
+  IDataObject,
+} from './types'
