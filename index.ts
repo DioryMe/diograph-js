099c203bbf20d6b53a3e83a88642c208507f799a
Jouni Alanen <jouni@diory.me>
2024-06-09 11:29:55 +0300

    Move dio-utils to diograph-js
    - constructAndLoadRoom, constructRoom and getClientAndVerify

--- index.ts
+++ index.ts
@@ -4,3 +4,9 @@ export { Diory } from './diory/diory'
 export { Room } from './diosphere/room'
 export { Connection } from './diosphere/connection'
 export { RoomClient } from './diosphere/roomClient'
+
+export {
+  constructAndLoadRoom,
+  constructRoom,
+  getClientAndVerify,
+} from './utils/constructAndLoadRoom'
---
+++ utils/constructAndLoadRoom.ts
@@ -0,0 +1,42 @@
+import { Room } from '../diosphere/room'
+import { RoomClient } from '../diosphere/roomClient'
+import { ConnectionClient, ConnectionClientList } from '../types'
+
+export const getClientAndVerify = async (
+  address: string,
+  clientType: string,
+  availableClients: ConnectionClientList,
+): Promise<ConnectionClient> => {
+  const clientData = availableClients[clientType]
+
+  if (!clientData) {
+    throw new Error(
+      `getClientAndVerify: Unknown clientType or incomplete availableClients data for ${clientType}`,
+    )
+  }
+
+  const client = new clientData.clientConstructor(address, clientData.credentials)
+  await client.verify()
+
+  return client
+}
+
+export const constructRoom = async (
+  address: string,
+  roomClientType: string,
+  availableClients: ConnectionClientList,
+): Promise<Room> => {
+  const client = await getClientAndVerify(address, roomClientType, availableClients)
+  const roomClient = new RoomClient(client)
+  return new Room(roomClient)
+}
+
+export const constructAndLoadRoom = async (
+  address: string,
+  roomClientType: string,
+  availableClients: ConnectionClientList,
+): Promise<Room> => {
+  const room = await constructRoom(address, roomClientType, availableClients)
+  await room.loadRoom(availableClients)
+  return room
+}
